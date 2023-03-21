2607d79af66ddeeea1e82dc850752a5e886a7e93
bilyanailonova <ilonova97@gmail.com>
2023-03-21 23:22:12 +0200

    Add Routes and Route to app

---
+++ client/app/src/App.js
@@ -0,0 +1,21 @@
+import {Routes,Route} from 'react-router-dom';
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
+import { Home } from './components/Home/Home';
+
+function App() {
+  return (
+    <div id="App">
+      <Header/>
+      <main id="main-content">
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </main>
+      <Footer/>
+      
+    </div>
+  );
+}
+
+export default App;
