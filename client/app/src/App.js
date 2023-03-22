c5b8333d0878999ff5edfdc9d3b730922d9af2c2
bilyanailonova <ilonova97@gmail.com>
2023-03-22 22:09:21 +0200

    Add Routes and Route

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -1,4 +1,4 @@
-import {Routes,Route} from 'react-router-dom';
+import { Routes,Route } from 'react-router-dom';
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 import { Home } from './components/Home/Home';
@@ -7,11 +7,11 @@ function App() {
   return (
     <div id="App">
       <Header/>
-      <main id="main-content">
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-        </Routes>
-      </main>
+       <main id="main-content"> 
+         <Routes> 
+         <Route path='/' element={<Home/>}/>
+         </Routes> 
+       </main> 
       <Footer/>
       
     </div>
